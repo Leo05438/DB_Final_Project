2570c1ab1ee48d9ffabbd0fd9486e675e5f33f8a
Leo05438 <05438leo@gmail.com>
2021-04-24 21:07:57 +0800

    connect to db

--- app/app.js
+++ app/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cookieSession = require('cookie-session');
+var mysql = require("mysql");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,12 +21,40 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// cookieSession
+app.use(cookieSession({
+    key: 'node',
+    secret: 'dfcsdfvsdfv'
+}))
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
+});
+
+
+// mysql
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "DB_project",
+    password: "12345",
+    port:3000,
+    database: "DB_project"
+});
+con.connect(function(err) {
+    if (err) {
+        console.log('connecting error');
+        return;
+    }
+    console.log('connecting success');
+});
+// db state
+app.use(function(req, res, next) {
+    req.con = con;
+    next();
 });
 
 // error handler
--- app/routes/users.js
+++ app/routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/register', function(req, res, next) {
+  res.render('users/register');
+});
+router.get('/signIn', function(req, res, next) {
+  res.render('users/signIn');
 });
 
 module.exports = router;

57b847552fb587b679ff91aae82470395fb06a34
Leo05438 <05438leo@gmail.com>
2021-04-25 01:56:14 +0800

    add signIn

--- app/routes/apis.js
+++ app/routes/apis.js
@@ -2,24 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/register',function(req,res,next){
-  //資料不完整
   if(req.session.signIned){
     res.redirect('/');
     return;
   }
+  //illegal data
   if((!req.body.user)||(!req.body.password)){
-    console.log('資料不完整');
+    console.log('illegal data');
     res.redirect('/users/register');
     return;
   }
-  //user重複
   req.con.query('SELECT COUNT(*) AS n FROM userPassword WHERE user=?',req.body.user,function(err,rows){
+    //user exist
     if(rows[0].n==1){
-      console.log('user重複');
+      console.log('user exist');
       res.redirect('/users/register');
       return;
     }
-    console.log('c3');
     //insert
     var data={
       user:req.body.user,
@@ -30,6 +29,42 @@ router.post('/register',function(req,res,next){
       req.session.signIned=true;
       res.redirect('/');
       return;
+    });
+  });
+});
+
+router.post('/signIn',function(req,res,next){
+  if(req.session.signIned){
+    res.redirect('/');
+    return;
+  }
+  //illegal data
+  if((!req.body.user)||(!req.body.password)){
+    console.log('illegal data');
+    res.redirect('/users/signIn');
+    return;
+  }
+  req.con.query('SELECT COUNT(*) AS n FROM userPassword WHERE user=?',req.body.user,function(err,rows){
+    //user not exist
+    if(rows[0].n==0){
+      console.log('user not exist');
+      res.redirect('/users/register');
+      return;
+    }
+    req.con.query('SELECT password FROM userPassword WHERE user=?',req.body.user,function(err,rows){
+      //wrong password
+      if(rows[0].password!=req.body.password){
+        console.log('wrong password');
+        res.redirect('/users/signIn');
+        return;
+      }
+      //signIn
+      else{
+        console.log('signIn');
+        req.session.signIned=true;
+        res.redirect('/');
+        return;
+      }
     });
   });
 });

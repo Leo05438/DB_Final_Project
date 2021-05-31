03e48f297ba65262de656075aac8ea7d4085b054
Alan0817 <71300392+Alan0817@users.noreply.github.com>
2021-05-31 17:51:46 +0800

    Add files via upload

---
+++ others_8_studios.sql
@@ -0,0 +1,23 @@
+WITH tmp AS (
+SELECT Studios, Premiered, COUNT(MAL_ID) AS cnt
+FROM anime
+WHERE Studios = "Sunrise" AND Premiered != "Unknown"
+GROUP BY Premiered 
+ORDER BY Premiered, cnt DESC
+),
+tmp1 AS (
+WITH t AS (
+SELECT Premiered, Score, A.Studios
+FROM anime A
+WHERE A.Studios IN (
+SELECT Studios FROM tmp
+)
+)
+SELECT t.Premiered, AVG(t.Score) AS avg_scr
+FROM t
+GROUP BY t.Premiered
+)
+SELECT cnt, AVG(avg_scr)
+FROM tmp, tmp1
+WHERE tmp1.Premiered = tmp.Premiered
+GROUP BY cnt

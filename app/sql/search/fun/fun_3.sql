87b8da480faab9f8f74d266130b9721e2c9990b6
Pooh1223 <leo891223@gmail.com>
2021-06-12 15:48:14 +0800

    Update fun_3

--- app/sql/search/fun/fun_3.sql
+++ app/sql/search/fun/fun_3.sql
@@ -3,10 +3,19 @@ FROM anime
 WHERE Name LIKE "%evangelion%"
 AND Aired LIKE "%19%";
 
-SELECT Name,Score
+SELECT Japanese_Name,Aired,Score
 FROM anime
-WHERE Aired LIKE "%1997%"
-AND Score >= 8.32;
+WHERE (Aired LIKE "%199%"
+OR Aired LIKE "%198%"
+OR Aired LIKE "%197%"
+OR Aired LIKE "%196%"
+OR Aired LIKE "%195%"
+OR Aired LIKE "%194%"
+OR Aired LIKE "%193%"
+OR Aired LIKE "%192%"
+OR Aired LIKE "%191%")
+AND Score >= 8.32
+ORDER BY Score DESC;
 -- = (SELECT Max(Score)
 -- FROM anime
 -- WHERE Aired LIKE "%1995%");
